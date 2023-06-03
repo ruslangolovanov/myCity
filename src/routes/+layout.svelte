cbd858829ec785813c931a064baf4cade40894ec
Ruslan <goloruslan64@gmail.com>
2023-06-03 16:37:49 +0300

    add main page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<nav>
+    <div class="nav-container">
+      <a href="/" class="nav-logo" title="Back to Homepage">Казань</a>
+         <div class="nav-links">
+             {#each nav as link}
+               {#if link.isShow}
+                  <a href = {link.href} class="link">{link.title}</a>
+               {/if}
+             {/each}
+         </div>
+    </div>
+</nav>
+
+<script>
+    import { each,  } from "svelte/internal";
+  
+      const nav = [
+          {
+              title : "Фотографии",
+              href : "/about",
+              isShow : true
+          },
+          {
+              title : "История",
+              href : "/blog",
+              isShow : true
+          },
+          {
+              title : "Достопримечательности",
+              href : "/project",
+              isShow : true
+          },
+          {
+              title : "Учебные заведения",
+              href : "/contact",
+              isShow : true
+          }
+      ]
+  </script>
+  
+
+<div class="container">
+    <!-- Pages will be injected below --> 
+    <slot></slot>
+</div>
+
+<style>
+    .container {
+    max-width: 1400px; margin: 50px auto;
+    }
+
+    nav{
+        background-color: #ecebe8;
+        width: 100%;
+        max-width: 1400px;
+        margin: 0 auto;
+        position: sticky;
+        border-radius: 10px;
+    }
+
+    .nav-logo{
+        margin: 0 60px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,83 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Ruslan</title>
+</svelte:head>
+
+<div class="container">
+    <!-- <div class="info">
+        <div class="animate-background">
+            <div class="container info-container">
+                <div class="info-text">
+                    <h1> 
+                        <p class="animate-text-1">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </h1>
+                </div>
+                <h2 class="subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. </h2> -->
+                <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+                <!-- <div style="color: black;" class="scroll-button animate-scroll" tabindex="0">
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </div> -->
+
+    <div class="title">
+        <h1>КАЗАНЬ</h1>
+    </div>
+    <div class="description">
+        Казань - город в котором я родился и живу, столица республики Татарстан, большой и развитый город. 
+        Здесь живет около 1,3 млн человек. Казань претендует на звание третьей столицы России и даже зарегистрировала одноименный бренд. 
+        По популярности у туристов столица Татарстана на четвертом месте в стране: в 2019 году ее посетили 3,5 млн человек, причем многие приезжали повторно.
+
+    </div>
+</div>
+<div class="background-div"></div>
+
+<style>
+    
+    .title{
+        display: flex;
+        justify-content: center;
+        background-image: url(./background.webp);
+        background-size: cover;
+        background-repeat: no-repeat;
+        border-radius: 20px;
+        margin: 10px;
+      
+    }
+
+    h1{
+        font-size: 200px;
+        color: rgba(0, 0, 0, 0.744);
+
+    }
+    .description{
+        background-color: #ecebe8;
+        font-size: 25px;
+        padding: 15px;
+        margin: 10px;
+        border-radius: 20px;
+    }
+
+    
+    
+
+    
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
